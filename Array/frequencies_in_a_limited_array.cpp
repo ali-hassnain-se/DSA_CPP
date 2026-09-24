8fb7b600b14867095138ec43daa1b9a25c644534
ali-hassnain-se <ranaalihassnain07@gmail.com>
2026-09-25 00:02:05 +0500

    Added problem Frequency Count in a limited array

--- Array/frequencies_in_a_limited_array.cpp
+++ Array/frequencies_in_a_limited_array.cpp
@@ -1,7 +1,13 @@
+/*
+    Problem Name: Frequencies in a Limited Array
+    Platform: GeeksforGeeks (GFG)
+    Problem Link: https://www.geeksforgeeks.org/problems/frequency-of-array-elements-1587115620/1?utm_medium=article_practice_tab&utm_campaign=article_practice_tab&utm_source=geeksforgeeks
+*/
 #include<iostream>
 #include<vector>
 using namespace std;
 
+// Defined Function
 vector<int> frequencyCount(vector<int>& arr);
 
 int main() {
@@ -25,6 +31,7 @@ int main() {
     return 0;
 }
 
+// Actual Code of problem
 vector<int> frequencyCount(vector<int>& arr) {
         // code here
 /*
@@ -46,7 +53,32 @@ First Method, Brute Force Approach, TC=O(N^2), SC=O(1)
         return ans;
 */
 
-// 2nd Method, Optimized Approach, TC=O(N), SC=O(1)
+/* 
+2nd Method, Better Than Brute Force Approach, TC=O(NlogN), SC=O(1)
+
+        int n=arr.size();
+        
+        sort(arr.begin(), arr.end());
+        
+        vector<int> ans(n, 0);
+        int count=1;
+        for(int i=1;i<n;i++) {
+            if(arr[i]==arr[i-1])
+                count++;
+            
+            else {
+                ans[arr[i-1]-1]=count;
+                count=1;
+            }
+        }
+        
+        ans[arr[n-1]-1]=count;
+        
+        return ans;
+*/
+
+/*
+3rd Method, Optimized Approach, TC=O(N), SC=O(1)
 
         int n=arr.size();
         
@@ -57,4 +89,26 @@ First Method, Brute Force Approach, TC=O(N^2), SC=O(1)
         }
         
         return ans;
+*/
+
+// 4th Method, Optimized Approach using store 2 numbers in one position
+// TC=O(N), SC=O(1)
+
+    int n=arr.size();
+
+    // decreasing all values by one
+    for(int i=0;i<n;i++)
+    arr[i]--;
+
+    // add n on every index
+    for(int i=0;i<n;i++)
+    arr[arr[i]%n]+=n;
+
+    vector<int> ans(n);
+    // 
+    for(int i=0;i<n;i++) {
+        ans[i]=arr[i]/n;
+    }
+
+    return ans;
 }
